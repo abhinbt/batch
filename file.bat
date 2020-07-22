71fdee297aa4f90ede3cdf50693a9cba1c7af5a2
ABHIN BT <abhinbt@gmail.com>
2020-07-22 18:37:10 +0530

    Update file.bat

--- file.bat
+++ file.bat
@@ -1,1 +1,11 @@
-echo "Hello World"
+@echo off
+SET /A c = %a%+%b%
+echo %c%
+SET /A c = %a%-%b%
+echo %c%
+SET /A c = %b%*%a%
+echo %c%
+SET /A c = %b%/%a%
+echo %c%
+SET /A c =%b% %% %a%
+echo %c%
